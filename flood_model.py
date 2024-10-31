7e0afe53c109e7fc4a2d940b9b90a3c93848dc3d
Parshuram Singh <parshuramsingh433@gmail.com>
2024-10-31 10:15:19 +0530

    accuracy

--- flood_model.py
+++ flood_model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import joblib
+from sklearn.metrics import r2_score
 
 # Load the dataset
 data = pd.read_csv("D:/Internship/Day-10-11th July/flood.csv")
@@ -17,5 +18,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = LinearRegression()
 model.fit(X_train, y_train)
 
+y_pred = model.predict(X_test)
+
+r2 = r2_score(y_test, y_pred) * 100
+
 # Save the trained model as a pickle file
 joblib.dump(model, 'flood_prediction_model.pkl')
+
+print(f"R^2 Score: {r2:.2f}%")
